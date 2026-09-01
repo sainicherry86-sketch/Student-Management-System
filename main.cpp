ef6e8c92c7cd000d49b902ce9d298e0037fca94d
Kanchan saini <sainicherry86@gmail.com>
2026-09-01 21:50:31 +0530

    Added basic C++ structure

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Student
+{
+public:
+    int id;
+    string name;
+    string course;
+    float marks;
+};
+
+int main()
+{
+    cout << "===== STUDENT MANAGEMENT SYSTEM =====" << endl;
+
+    return 0;
+}
